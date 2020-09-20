4937ea17ec1cc489e36f079291bec959048f3512
Uriel Ortega <uortega4563@gmail.com>
2020-09-20 01:27:02 +0000

    Added Taking CS407 because

--- myNameIs.java
+++ myNameIs.java
@@ -2,5 +2,6 @@ public class myNameIs{
 	public static void main(String[] args){
 		System.out.println("My Name Is Uriel");
 		System.out.println("My favorite programming language is Java");
+		System.out.println("I am taking CS407 because I want to develop my Linux knowledge");
 	}
 }
